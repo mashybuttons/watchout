c4558d04181cfcb1568d8a3341677a5e63f7b8ab
mashybuttons <katherine.sj@gmail.com>
2016-04-29 21:13:38 -0700

    added movecircle function

--- client/watchout.js
+++ client/watchout.js
@@ -1,40 +1,59 @@
 // start slingin' some d3 here.
 
-var alive = true;
-var highscore = 0;
-var score = 0;
-var collisions = 0;
+
+var player = d3.select('.player');
+var enemy = d3.select('.circ');
 
 var svg = d3.select('body')
   .append('svg')
   .attr({'width': 800, 'height': 500})
   .style('border', '2px solid black');
 
-var randomCXorCY = function(n) {
+// var randomCXorCY = function(n) {
+//   var result = [];
+//   for (var i = 0; i < n; i++) {
+//     result[i] = Math.floor(Math.random() * 800) + 20;
+//   }
+//   return result;
+// };
+
+var makeCircle = function (n) {
   var result = [];
   for (var i = 0; i < n; i++) {
-    result[i] = Math.floor(Math.random() * 800) + 20;
+    result.push({x: Math.floor(Math.random() * 800) + 20,
+                  y: Math.floor(Math.random() * 480) + 20});
   }
   return result;
 };
 
 var distanceBetween = function (playX, playY, enemX, enemY) {
-  console.log(Math.sqrt(Math.pow(playX - enemX, 2) + Math.pow(playY - enemY, 2)));
+  return Math.sqrt(Math.pow(playX - enemX, 2) + Math.pow(playY - enemY, 2));
 };
 
 var circles = svg.selectAll('circle')
-  .data(randomCXorCY(5))
+  .data(makeCircle(5))
   .enter()
-  .append('circle');
+  .append('circle')
+  .attr('r', 10)
+  .attr('class', 'circ');
 
-setInterval(function() {
+// setInterval(function() {
+//   circles.transition().attr('cx', function() { return Math.floor(Math.random() * 780) + 20; })
+//     .attr('cy', function() { return Math.floor(Math.random() * 480) + 20; })
+//     .attr('r', 10)
+//     .attr('class', 'circ');
+// }, 1000);
+
+var moveCircles = function() {
   circles.transition().attr('cx', function() { return Math.floor(Math.random() * 780) + 20; })
     .attr('cy', function() { return Math.floor(Math.random() * 480) + 20; })
-    .attr('r', 10)
-    .attr('class', 'circ');
-}, 1000);
-var player = d3.select('.player');
-var enemy = d3.select('.circ');
+      .each('end', function() {
+        d3.select(this).transition().attr('cx', function() { return Math.floor(Math.random() * 780) + 20; })
+         .attr('cy', function() { return Math.floor(Math.random() * 480) + 20; })
+          .each('end', function() { moveCircles(); });
+      });
+};
+
 
 var drag = d3.behavior.drag()  
   .on('drag', function() { 
@@ -44,7 +63,7 @@ var drag = d3.behavior.drag()
       // .call(distanceBetween(d3.select('.player').attr('cx'), d3.select('.player').attr('cy'), d3.select('.circ').attr('cx'), d3.select('.circ').attr('cy')));
   });
 svg.selectAll('circle')
-  .data(randomCXorCY(6))
+  .data(makeCircle(6))
   .enter()
   .append('circle')
   .attr('cx', function() { return Math.floor(Math.random() * 780) + 20; })
@@ -53,31 +72,39 @@ svg.selectAll('circle')
   .attr('class', 'player')
   .call(drag);
 
-  
-// while (alive) {
-setInterval(function () {
-  score++;
-  if (score >= highscore) {
-    highscore++;
-  }
-  
-d3.select('body')
-  .select('.scoreboard')
-  .selectAll('span')
-  .data([highscore, score, collisions])
-  .text(function (d) {return d});
-  
-}, 1000);
-  // for (var i = 0; i < 5; i++) {
-  //  console.log(i);
-  //   if (distanceBetween(d3.select('.player').attr('cx'), d3.select('.player').attr('cy'), d3.select('body').each('.circ').attr('cx'), d3.select('body').each('.circ').attr('cy')) <= 20) {
-  //     score = 0;
-  //     collisions++;
-  //     alive = false;
-    // }
-  // }
-// }
 
+var startGame = function() {
+  var highscore = 0;
+  var score = 0;
+  var collisions = 0;
+  moveCircles();
+ 
+// while (alive) {
+  setInterval(function () {
+    score++;
+    if (score >= highscore) {
+      highscore++;
+    }
+    
+    d3.select('body')
+      .select('.scoreboard')
+      .selectAll('span')
+      .data([highscore, score, collisions])
+      .text(function (d) { return d; });
+
+    circles.each(function(d) {
+      var enemy = d3.select(this);
+      
+      if (distanceBetween(d3.select('.player').attr('cx'), d3.select('.player').attr('cy'), enemy.attr('cx'), enemy.attr('cy')) <= 20) {
+        collisions++;
+        score = 0;
+      }
+    });
+    
+  }, 1000);
+};
+
+startGame();
 
 
 
